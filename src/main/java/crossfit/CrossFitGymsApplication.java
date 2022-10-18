74572da755d779f56fe9b39ef824aa2a15852ca5
Tony Ehlert <aehlert@dmacc.edu>
2022-10-18 08:25:55 -0500

    Update CrossFitGymsApplication.java
    Removed the CommandLineRunner implementation from class and commented out the AutoWiring of the repository and the run method.

--- src/main/java/crossfit/CrossFitGymsApplication.java
+++ src/main/java/crossfit/CrossFitGymsApplication.java
@@ -15,7 +15,7 @@ import crossfit.controller.BeanConfiguration;
 import crossfit.repository.GymRepository;
 
 @SpringBootApplication
-public class CrossFitGymsApplication implements CommandLineRunner{
+public class CrossFitGymsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrossFitGymsApplication.class, args);
@@ -28,28 +28,28 @@ public class CrossFitGymsApplication implements CommandLineRunner{
 		
 	}
 	
-	@Autowired
-	GymRepository repo;
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
-		
-		//Use an existing bean
-		Gym g = appContext.getBean("gym", Gym.class);
-		g.setAffiliated(true);
-		g.setDateOpened(LocalDate.of(2010, 01, 01));
-		repo.save(g);
-		
-		//Creating a bean to use - not managed by Spring
-		Gym northAnkeny = new Gym("North Ankeny CrossFit", LocalDate.of(2012, 06, 10), false);
-		repo.save(northAnkeny);
-		
-		List<Gym> allGyms = repo.findAll();
-		for(Gym gyms: allGyms) {
-			System.out.println(gyms.toString());
-		}
-	}
+//	@Autowired
+//	GymRepository repo;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		// TODO Auto-generated method stub
+//		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+//		
+//		//Use an existing bean
+//		Gym g = appContext.getBean("gym", Gym.class);
+//		g.setAffiliated(true);
+//		g.setDateOpened(LocalDate.of(2010, 01, 01));
+//		repo.save(g);
+//		
+//		//Creating a bean to use - not managed by Spring
+//		Gym northAnkeny = new Gym("North Ankeny CrossFit", LocalDate.of(2012, 06, 10), false);
+//		repo.save(northAnkeny);
+//		
+//		List<Gym> allGyms = repo.findAll();
+//		for(Gym gyms: allGyms) {
+//			System.out.println(gyms.toString());
+//		}
+//	}
 
 }
