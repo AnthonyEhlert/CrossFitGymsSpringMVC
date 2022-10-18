bc60ba2ceabee1a0fed0f166d49b08438d388d60
Tony Ehlert <aehlert@dmacc.edu>
2022-10-18 17:35:11 -0500

    Update BeanConfiguration.java

--- src/main/java/crossfit/controller/BeanConfiguration.java
+++ src/main/java/crossfit/controller/BeanConfiguration.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import crossfit.beans.Coach;
 import crossfit.beans.Gym;
 
 /**
@@ -21,6 +22,12 @@ public class BeanConfiguration {
 //		bean.setName("CrossFit Ankeny");
 //		bean.setDateOpened(LocalDate.of(2010, 01, 01));
 //		bean.setAffiliated(true);
+		return bean;
+	}
+	
+	@Bean
+	public Coach coach() {
+		Coach bean = new Coach("No First Name Given", "No Last Name Given");
 		return bean;
 	}
 }
