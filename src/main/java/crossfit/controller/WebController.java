9f5ae584e3adad94c34d48dce93bcaa8aeae9e26
Tony Ehlert <aehlert@dmacc.edu>
2022-10-19 16:19:12 -0500

    Update WebController.java

--- src/main/java/crossfit/controller/WebController.java
+++ src/main/java/crossfit/controller/WebController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import crossfit.beans.Certification;
+import crossfit.beans.Coach;
 import crossfit.beans.Gym;
+import crossfit.repository.CertificationRepository;
+import crossfit.repository.CoachRepository;
 import crossfit.repository.GymRepository;
 
 /**
@@ -21,12 +25,18 @@ import crossfit.repository.GymRepository;
 public class WebController {
 
 	@Autowired
-	GymRepository repo;
+	GymRepository gymRepo;
+	
+	@Autowired
+	CoachRepository coachRepo;
+	
+	@Autowired
+	CertificationRepository certRepo;
 	
 	@GetMapping("/viewAllGyms")
 	public String viewAllGyms(Model model) {
-		model.addAttribute("gyms", repo.findAll());
-		return "gymresults";
+		model.addAttribute("gyms", gymRepo.findAll());
+		return "gymResults";
 	}
 	
 	@GetMapping("inputGym")
@@ -38,28 +48,106 @@ public class WebController {
 	
 	@PostMapping("/inputGym")
 	public String addNewGym(@ModelAttribute Gym g, Model model) {
-		repo.save(g);
+		gymRepo.save(g);
 		return viewAllGyms(model);
 	}
 	
 	@GetMapping("/editGym/{id}")
 	public String showUpdateGym(@PathVariable("id") long id, Model model) {
-		Gym g = repo.findById(id).orElse(null);
+		Gym g = gymRepo.findById(id).orElse(null);
 		model.addAttribute("newGym", g);
 		return "inputGym";
 	}
 	
-	@PostMapping("/update/{id}")
+	@PostMapping("/updateGym/{id}")
 	public String reviseGym(Gym g, Model model) {
-		repo.save(g);
+		gymRepo.save(g);
 		return viewAllGyms(model);
 	}
 	
 	@GetMapping("/deleteGym/{id}")
 	public String deleteGym(@PathVariable("id") long id, Model model) {
-		Gym g = repo.findById(id).orElse(null);
-		repo.delete(g);
+		Gym g = gymRepo.findById(id).orElse(null);
+		gymRepo.delete(g);
 		return viewAllGyms(model);
+	}
+	
+	@GetMapping("/viewAllCoaches")
+	public String viewAllCoaches(Model model) {
+		model.addAttribute("coaches", coachRepo.findAll());
+		return "coachResults";
+	}
+	
+	@GetMapping("inputCoach")
+	public String addNewCoach(Model model) {
+		Coach c = new Coach();
+		model.addAttribute("newCoach", c);
+		return "inputCoach";
+	}
+	
+	@PostMapping("/inputCoach")
+	public String addNewCoach(@ModelAttribute Coach c, Model model) {
+		coachRepo.save(c);
+		return viewAllCoaches(model);
+	}
+	
+	@GetMapping("/editCoach/{id}")
+	public String showUpdateCoach(@PathVariable("id") long id, Model model) {
+		Coach c = coachRepo.findById(id).orElse(null);
+		model.addAttribute("newCoach", c);
+		return "inputCoach";
+	}
+	
+	@PostMapping("/updateCoach/{id}")
+	public String reviseCoach(Coach c, Model model) {
+		coachRepo.save(c);
+		return viewAllCoaches(model);
+	}
+	
+	@GetMapping("/deleteCoach/{id}")
+	public String deleteCoach(@PathVariable("id") long id, Model model) {
+		Coach c = coachRepo.findById(id).orElse(null);
+		coachRepo.delete(c);
+		return viewAllCoaches(model);
+	}
+	
+	@GetMapping("/viewAllCertifications")
+	public String viewAllCertifications(Model model) {
+		model.addAttribute("certifications", certRepo.findAll());
+		return "certResults";
+	}
+	
+	@GetMapping("inputCertification")
+	public String addNewCertification(Model model) {
+		Certification cert = new Certification();
+		model.addAttribute("newCertification", cert);
+		return "inputCertification";
+	}
+	
+	@PostMapping("/inputCertification")
+	public String addNewCertification(@ModelAttribute Certification cert, Model model) {
+		certRepo.save(cert);
+		return viewAllCertifications(model);
+	}
+	
+	@GetMapping("/editCertification/{id}")
+	public String showUpdateCertification(@PathVariable("id") long id, Model model) {
+		Certification cert = certRepo.findById(id).orElse(null);
+		model.addAttribute("newCertification", cert);
+		return "inputCertification";
+	}
+	
+	@PostMapping("/updateCertification/{id}")
+	public String reviseCertification(Certification cert, Model model) {
+		certRepo.save(cert);
+		return viewAllCertifications(model);
+	}
+	
+	@GetMapping("/deleteCertification/{id}")
+	public String deleteCertification(@PathVariable("id") long id, Model model) {
+		Certification cert = certRepo.findById(id).orElse(null);
+		certRepo.delete(cert);
+		return viewAllCertifications(model);
 	}
 	
 }
