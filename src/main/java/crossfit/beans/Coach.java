6d940897cc4150db81dce1452219b042524a71d7
Tony Ehlert <aehlert@dmacc.edu>
2022-10-24 09:56:14 -0500

    Updated Gym/Coach Classes
    Updated for Lombok

--- src/main/java/crossfit/beans/Coach.java
+++ src/main/java/crossfit/beans/Coach.java
@@ -12,6 +12,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 /**
  * @author Tony Ehlert - aehlert
@@ -19,6 +22,8 @@ import javax.persistence.Table;
  * Oct 18, 2022
  */
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name= "coach")
 public class Coach {
@@ -38,78 +43,78 @@ public class Coach {
 	private List<Certification> listOfCertifications;
 	
 	
-	//default no-arg constructor
-	public Coach() {
-		super();
-	}
-	
-	//helper constructors
-	public Coach(long id, String firstName, String lastName, Gym gym) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gym = gym;
-	}
-	
-	public Coach(String firstName, String lastName, Gym gym) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gym = gym;
-	}
-	
-	public Coach(String firstName, String lastName) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-	
-	
-	//getters and setters
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Gym getGym() {
-		return gym;
-	}
-
-	public void setGym(Gym gym) {
-		this.gym = gym;
-	}
-
-	public List<Certification> getListOfCertifications() {
-		return listOfCertifications;
-	}
-
-	public void setListOfCertifications(List<Certification> listOfCertifications) {
-		this.listOfCertifications = listOfCertifications;
-	}
-
-	@Override
-	public String toString() {
-		return "Coach [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", gym=" + gym + "]";
-	}
+//	//default no-arg constructor
+//	public Coach() {
+//		super();
+//	}
+//	
+//	//helper constructors
+//	public Coach(long id, String firstName, String lastName, Gym gym) {
+//		super();
+//		this.id = id;
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.gym = gym;
+//	}
+//	
+//	public Coach(String firstName, String lastName, Gym gym) {
+//		super();
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.gym = gym;
+//	}
+//	
+//	public Coach(String firstName, String lastName) {
+//		super();
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//	}
+//	
+//	
+//	//getters and setters
+//	public long getId() {
+//		return id;
+//	}
+//
+//	public void setId(long id) {
+//		this.id = id;
+//	}
+//
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public void setFirstName(String firstName) {
+//		this.firstName = firstName;
+//	}
+//
+//	public String getLastName() {
+//		return lastName;
+//	}
+//
+//	public void setLastName(String lastName) {
+//		this.lastName = lastName;
+//	}
+//
+//	public Gym getGym() {
+//		return gym;
+//	}
+//
+//	public void setGym(Gym gym) {
+//		this.gym = gym;
+//	}
+//
+//	public List<Certification> getListOfCertifications() {
+//		return listOfCertifications;
+//	}
+//
+//	public void setListOfCertifications(List<Certification> listOfCertifications) {
+//		this.listOfCertifications = listOfCertifications;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Coach [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", gym=" + gym + "]";
+//	}
 	
 }
